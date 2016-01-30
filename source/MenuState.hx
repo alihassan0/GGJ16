ba7012f9ad9af0a31f0d9f4e05e931891d556629
ali hassan <alithephantom@gmail.com>
2016-01-30 23:02:34 +0200

    Added good enough lightning for now

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -5,6 +5,8 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
+import flixel.math.FlxPoint;
+import flixel.addons.display.shapes.FlxShapeLightning;
 
 using flixel.util.FlxSpriteUtil;
 
@@ -15,6 +17,8 @@ class MenuState extends FlxState
 {
 	private var gridSprite:FlxSprite;
 	private var grid:Grid;
+	private var lightning:FlxShapeLightning;
+
 	override public function create():Void
 	{
 		this.bgColor = 0xFFBBDEFB;
@@ -31,8 +35,10 @@ class MenuState extends FlxState
 									 0,0,0,0,0,0,
 									  0,0,0,0,0,0];
 		grid.loadArray(level1Data);
+		//var lightningStyle = { color: 0xFFFF0000, thickness: 3.0 };
+		var lightningStyle:LightningStyle = {thickness: 3.0,displacement: 150,detail:2 ,color:0xFFFFFFFF};
+		add(lightning = new FlxShapeLightning(0,0,new FlxPoint(0,0),new FlxPoint(250,250),lightningStyle));
 	}
-	
 		
 	
 	/**
@@ -50,5 +56,6 @@ class MenuState extends FlxState
 	override public function update(t:Float):Void
 	{
 		super.update(t);
+		lightning.recalculate(new FlxPoint(0,0),new FlxPoint(250,250),50,0);
 	}	
 }
